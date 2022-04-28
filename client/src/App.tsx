964d996a188ee8fe7c9462cd67fc3e38906c39c5
mnmhbbb <mnmhb64@gmail.com>
2022-04-29 01:28:35 +0900

    deploy test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>이미지 풀스택</div>;
+  return <div className='App'>헤로쿠 배포 테스트</div>;
 }
 
 export default App;
